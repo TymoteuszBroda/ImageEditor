b765f080a03b29b9246f3be15b9702cf4f30a2a8
Tymotesuz Broda <tymoteusz.91100@gmail.com>
2023-03-05 12:56:57 +0100

    added funcionality to create necessary directories before executinf for loop

--- ImageEditor.py
+++ ImageEditor.py
@@ -1,5 +1,21 @@
 from PIL import Image, ImageEnhance, ImageFilter
 import os
 
-print("test")
-print("tymek")
+path = './imgs'
+pathOut = '/editedImgs'
+
+os.mkdir(path)
+os.mkdir(pathOut)
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+
+    edit = img.filter(ImageFilter.SHARPEN).convert('L') 
+
+    factor = 1.5
+    enchancer = ImageEnhance.Contrast(edit)
+    edit = enchancer.enhance(factor)
+
+    clean_name = os.path.splitext(filename) [0]
+
+    edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
